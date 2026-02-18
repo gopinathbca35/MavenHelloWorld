f091a63ff29d4bb2bbaa48d746e979f6bed0f7f1
Gopinath L <gopinathbca35@gmail.com>
2026-02-18 15:05:26 +0530

    Version 0.2.1

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "DevOps Project for DevOps Vijay Demo testing";
+        return "DevOps Project for DevOps Gopinath L Demo testing";
     }
 }
